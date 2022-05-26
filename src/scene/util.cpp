b3ef34d431d36c8e9526fd757428ed6abddab74c
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2022-05-27 00:34:19 +0700

    Scene print image

--- src/scene/util.cpp
+++ src/scene/util.cpp
@@ -11,13 +11,15 @@ const char* get_url_string(uint32_t id) {
 
 obs_source_t* create_browser_source(const char* link) {
   blog(LOG_INFO, "Creating browser source with link %s", link);
-  const char* browser = obs_module_text("color_source");
+  const char* browser = obs_module_text("image_source");
 
   blog(LOG_INFO, "Browser id: %s", browser);
 
   obs_data_t *settings = obs_get_source_defaults(browser);
   obs_source_t *source;
 
+//  obs_data_set_int(settings, "color", 0xF47C7Cff);
+  obs_data_set_string(settings, "file", "/home/bayus/Pictures/bikinibottom.jpg");
   source = obs_source_create_private(browser, nullptr, settings);
 
   obs_data_release(settings);
